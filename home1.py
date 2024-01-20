39b282561949fae7fd586e956381ce898d97c0b0
Astomarin <propozzzion@gmail.com>
2024-01-20 16:58:14 -0500

    home work 2

--- home1.py
+++ home1.py
@@ -1,1 +1,15 @@
-print("Hi Hillel IT")
+def print_digits(number):
+    if not (isinstance(number, int) and 1000 <= number <= 9999):
+        print("Треба ввести 4-ох значне число")
+        return
+
+    print(number // 1000)
+    print((number % 1000) // 100)
+    print((number % 100) // 10)
+    print(number % 10)
+
+try:
+    user_input = int(input("Введіть 4-ох значне число: "))
+    print_digits(user_input)
+except ValueError:
+    print("Помилка, це не 4-ох значне число")
