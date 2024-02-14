5cc7c2aa583a1b431a9029e35760243eb2ba30fb
Astomarin <propozzzion@gmail.com>
2024-02-14 18:31:00 -0500

    -

--- Homework 6 ( task 2).py
+++ Homework 6 ( task 2).py
@@ -3,8 +3,8 @@ my_dict_2 = {11: 11, 2: 22, 3: 33}
 
 
 common_keys = list(set(my_dict_1.keys()) & set(my_dict_2.keys()))
-unique_keys_1 = list(set(my_dict_1.keys()) - set(my_dict_2.keys()))
-new_dict = {key: my_dict_1[key] for key in unique_keys_1 if key not in my_dict_2}
+first_dict_keys = list(set(my_dict_1.keys()) - set(my_dict_2.keys()))
+new_dict = {key: my_dict_1[key] for key in first_dict_keys if key not in my_dict_2}
 merged_dict = {}
 
 for key in my_dict_1.keys():
@@ -19,6 +19,6 @@ for key in my_dict_2.keys():
 
 
 print("а) Список з ключами, які є в обох діктах:", common_keys)
-print("б) Список ключей, які є в першому але немає в другому", unique_keys_1)
+print("б) Список ключей, які є в першому але немає в другому", first_dict_keys)
 print("в) Новий словник з ключами, які є в першому, але немає в другому словнику:", new_dict)
 print("г) Об'єднані словники:", merged_dict)
